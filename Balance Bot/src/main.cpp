58cfc233c98d85ce49366b8cec085560c6fb73cf
Arthur Aznive <61330561+Amoskeag@users.noreply.github.com>
2023-03-11 14:06:22 -0500

    Add LSM9DS1 Library
    Include sample code for reading Gyroscope data from the chip.

--- Balance Bot/src/main.cpp
+++ Balance Bot/src/main.cpp
@@ -1,9 +1,45 @@
+/*
+  BALANCE BOT 
+  PROGRAMMER: Arthur W. Aznive Jr.
+  DATE:       3/11/2023
+  NOTES:      Created for SNHU Control Systems Analysis Fall 2023
+              Using the LSM9DS1 sensor.
+  VERSION:    0.1
+*/
+
 #include <Arduino.h>
+
+#include <Arduino_LSM9DS1.h>
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("Started");
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+  Serial.print("Gyroscope sample rate = ");
+  Serial.print(IMU.gyroscopeSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Gyroscope in degrees/second");
+  Serial.println("X\tY\tZ");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  float x, y, z;
+
+  if (IMU.gyroscopeAvailable()) {
+    IMU.readGyroscope(x, y, z);
+
+    Serial.print(x);
+    Serial.print('\t');
+    Serial.print(y);
+    Serial.print('\t');
+    Serial.println(z);
+  }
 }
